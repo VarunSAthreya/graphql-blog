52c5bc631281f118c08b4368255cfd558d28c0fd
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-30 01:18:18 +0530

    Updaetd Types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,9 +1,40 @@
 interface IPost {
+    author: IAuthor;
+    createdAt: string;
+    slug: string;
     title: string;
     excerpt: string;
+    featuredImage: {
+        url: string;
+    };
+    categories: ICategory[];
+}
+
+interface IResponsePost {
+    cursor: string;
+    node: {
+        author: IAuthor;
+        createdAt: string;
+        slug: string;
+        title: string;
+        excerpt: string;
+        featuredImage: {
+            url: string;
+        };
+        categories: ICategory[];
+    };
 }
 
 interface ICategory {
     name: string;
     slug: string;
+}
+
+interface IAuthor {
+    bio: string;
+    id: string;
+    name: string;
+    photo: {
+        url: string;
+    };
 }
