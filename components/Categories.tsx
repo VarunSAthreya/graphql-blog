859dcccd22ebd0eef75ac07a9fcd5db7cfe400a6
Varun S Athreya <varun.sathreya@gmail.com>
2021-12-02 21:10:11 +0530

    Added category side menu

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,7 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import Link from 'next/link';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { getCategories } from '../services';
 
 const Categories: FunctionComponent = () => {
-    return <div>Categories</div>;
+    const [categories, setCategories] = useState<ICategory[]>([]);
+
+    useEffect(() => {
+        getCategories().then((newCategories) => setCategories(newCategories));
+    }, []);
+
+    return (
+        <div className="p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg">
+            <h3 className="mb-8 text-xl font-semibold border-b pb 4">
+                Categories
+            </h3>
+            {categories.map((category) => (
+                <Link key={category.slug} href={`/category/${category.slug}`}>
+                    <span className="block pb-3 mb-3 cursor-pointer">
+                        {category.name}
+                    </span>
+                </Link>
+            ))}
+        </div>
+    );
 };
 
 export default Categories;
