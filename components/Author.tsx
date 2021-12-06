00efcae6be4f5f4e4e60a1993016b6e4d7fba488
Varun S Athreya <varun.sathreya@gmail.com>
2021-12-06 23:05:41 +0530

    Added author section

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import { FunctionComponent } from 'react';
 
-const Author: FunctionComponent = () => {
+type Props = {
+    author: IAuthor;
+};
+
+const Author: FunctionComponent<Props> = ({ author }: Props) => {
     return (
-        <div>
-            <h1>Author</h1>
+        <div className="relative p-12 mt-20 mb-8 text-center bg-black rounded-lg bg-opacity-20">
+            <div className="absolute left-0 right-0 -top-14">
+                <Image
+                    src={author.photo.url}
+                    unoptimized
+                    className="align-middle rounded-full"
+                    alt={author.name}
+                    height="100px"
+                    width="100px"
+                />
+            </div>
+            <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+            <p className="text-lg text-white">{author.bio}</p>
         </div>
     );
 };
